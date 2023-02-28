fffe207b18fa899ba7db9b139b76507a9bd6799f
Daniel <danielaular2010@gmail.com>
2023-02-28 11:15:34 -0500

    proyecto de notas iniciado

---
+++ src/DevolviendoPromediosSobreDiferentesNotas.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class DevolviendoPromediosSobreDiferentesNotas {
+    public static void main(String[] args) {
+
+        Scanner scanner= new Scanner(System.in);
+
+        double nota;
+        int contadorDeNotasMayoresA5 = 0;
+        int contadorDeNotasMenoresA4= 0;
+        int contadorDeNotas1 = 0;
+        double sumaDeLasNotasMayoresA5 =0;
+        double sumaDeLasNotasMenoresA4 =0;
+        double sumaDeLasNotasTotales =0;
+        double promedioDeNotasMayoresA5, promedioDeNotasMenoresA4;
+        boolean error = false;
+
+        for (int i = 0; i < 20; i ++){
+            System.out.printf("Introduzca por favor 20 notas entre 1 y 7 " +(i + 1)+ ": " );
+            nota = scanner.nextDouble();
+
+        }
+
+
+
+
+
+
+    }
+}
