3a1026c448519b38178ce5923bd02a41aebfd6a1
Daniel <danielaular2010@gmail.com>
2023-02-28 10:56:31 -0500

    arreglado el programa

--- src/DevuelveElMenorDeDiezEnteros.java
+++ src/DevuelveElMenorDeDiezEnteros.java
@@ -1,21 +1,23 @@
+import java.util.Scanner;
+
 public class DevuelveElMenorDeDiezEnteros {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.printf("ingresa 10 numeros enteros");
-        int cantidad = scanner.nexInt;
-
-        if (cantidad < 10){
+        System.out.println("en este programa introduciras 10 cantidades entera ");
+        int cantidad = scanner.nextInt();
+        if (cantidad < 10) {
             System.out.printf("error deben ser 10 cantidades");
-        }else {
-           int menor = Integer.MAX_VALUE;
-           int numero;
-            for (int i = 0; i < cantidad; i++){
-                System.out.printf("ingrese su numero "+(i + 1)+ "por favor: ");
-                 numero = scanner.nexInt;
-                 int menor = numero < menor ? numero : numeroMaximo;
+        } else {
+            int menor = Integer.MAX_VALUE;
+            int numero;
+            for (int i = 0; i < cantidad; i++) {
+                System.out.printf("ingrese su numero " + ( i +1 ) + "por favor: ");
+                numero = scanner.nextInt();
+                menor = numero < menor ? numero : menor;
             }
-            System.out.printf("el numero menor es el : " + menor );
-            if (menor < 10) System.out.printf("el numero menor es menor que 10");
-            else "el numero es mayor o igual a 10"
+            System.out.printf("el numero menor es el:  " + menor);
+            if (menor < 10) System.out.printf("el numero menor es menor que 10 ");
+            else System.out.printf("el numero es mayor o igual a 10");
+        }
     }
 }
